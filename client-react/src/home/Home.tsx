a04603b24fe5b345ab57bc51b9aa3c7cbdcf5d66
sheldon <sheldonlau1@gmail.com>
2024-07-05 18:40:26 -0700

    fix button alignment

--- client-react/src/home/Home.tsx
+++ client-react/src/home/Home.tsx
@@ -1,31 +1,25 @@
+import InstructionDialog from "../dialogs/instruction-dialog/InstructionDialog";
 import LobbyDialog, {
   LobbyDialogType,
 } from "../dialogs/lobby-dialog/LobbyDialog";
 import "./Home.less";
-import { Button } from "@mui/material";
 
 export default function Home() {
   return (
     <>
       <div className="homepage">
         <div className="page-body">
-          <div className="buttons">
-            <LobbyDialog
-              onSubmit={() => null}
-              dialogType={LobbyDialogType.CREATE}
-            ></LobbyDialog>
-            <LobbyDialog
-              onSubmit={() => null}
-              dialogType={LobbyDialogType.JOIN}
-            ></LobbyDialog>
-            <div>
-              <Button
-                className="how-to-button"
-                variant="contained"
-                // (click)="playVideo()"
-              >
-                How to Play
-              </Button>
+          <div className="buttons-container">
+            <div className="buttons">
+              <LobbyDialog
+                onSubmit={() => null}
+                dialogType={LobbyDialogType.CREATE}
+              ></LobbyDialog>
+              <LobbyDialog
+                onSubmit={() => null}
+                dialogType={LobbyDialogType.JOIN}
+              ></LobbyDialog>
+              <InstructionDialog></InstructionDialog>
             </div>
           </div>
         </div>
