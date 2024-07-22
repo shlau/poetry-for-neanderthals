d098c4115d198f900e88bf88a3ddd3841c4cadda
sheldon <sheldonlau1@gmail.com>
2024-07-21 17:45:58 -0700

    added lobby tests

---
+++ client-react/src/lobby/Lobby.test.tsx
@@ -0,0 +1,97 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+import Lobby from "./Lobby";
+import { User } from "../models/User.model";
+import "@testing-library/jest-dom";
+
+describe("Lobby", () => {
+  const currentUser = { id: "1", name: "user 1" } as User;
+  const users: User[] = [currentUser, { id: "2", name: "user 2" } as User];
+  const sendMessageSpy = vi.fn();
+
+  describe("start button", () => {
+    it("should be disabled if no users are ready", () => {
+      render(
+        <Lobby
+          sendMessage={sendMessageSpy}
+          users={users}
+          currentUser={currentUser}
+        />
+      );
+      expect(screen.getByText("Start game").closest("button")).toHaveAttribute(
+        "disabled"
+      );
+    });
+
+    it("should not be disabled if all users are ready", () => {
+      render(
+        <Lobby
+          sendMessage={sendMessageSpy}
+          users={[{ id: "1", name: "user 1", ready: true } as User]}
+          currentUser={currentUser}
+        />
+      );
+      expect(
+        screen.getByText("Start game").closest("button")
+      ).not.toHaveAttribute("disabled");
+    });
+  });
+
+  describe("ready button", () => {
+    it("should be disabled if not on a team", () => {
+      render(
+        <Lobby
+          sendMessage={sendMessageSpy}
+          users={[{ id: "1", name: "user 1", team: "unassigned" } as User]}
+          currentUser={currentUser}
+        />
+      );
+      expect(screen.getByText("Ready").closest("button")).toHaveAttribute(
+        "disabled"
+      );
+    });
+
+    it("should not be disabled if on a team", () => {
+      render(
+        <Lobby
+          sendMessage={sendMessageSpy}
+          users={[{ id: "1", name: "user 1", team: "blue" } as User]}
+          currentUser={currentUser}
+        />
+      );
+      expect(screen.getByText("Ready").closest("button")).not.toHaveAttribute(
+        "disabled"
+      );
+    });
+  });
+
+  describe("check mark", () => {
+    it("should be visible if on a team and ready", () => {
+      render(
+        <Lobby
+          sendMessage={sendMessageSpy}
+          users={[
+            { id: "1", name: "user 1", team: "blue", ready: true } as User,
+          ]}
+          currentUser={currentUser}
+        />
+      );
+
+      expect(screen.getAllByTestId("CheckIcon")).toBeDefined();
+    });
+
+    it("should not be visible if on a team and not ready", () => {
+      render(
+        <Lobby
+          sendMessage={sendMessageSpy}
+          users={[
+            { id: "1", name: "user 1", team: "blue", ready: false } as User,
+          ]}
+          currentUser={currentUser}
+        />
+      );
+
+      expect(screen.queryByTestId("CheckIcon")).toBeNull();
+    });
+  });
+});
--- client-react/src/services/api/GamesService.test.tsx
+++ client-react/src/services/api/GamesService.test.tsx
@@ -42,7 +42,7 @@ describe("Games Service", () => {
   describe("join game", () => {
     it("sends post request with given inputs", () => {
       const name = "name";
-      const gameId = 1;
+      const gameId = "1";
       const jsonBody = JSON.stringify({ name, gameId });
       joinGame(name, gameId);
       expect(fetchSpy).toHaveBeenCalledWith("/api/join", {
@@ -53,7 +53,7 @@ describe("Games Service", () => {
 
     it("throws error if response is not okay", () => {
       const name = "name";
-      const gameId = 1;
+      const gameId = "1";
       fetchSpy.mockReturnValue({ ok: false, status: "404" });
       expect(() => joinGame(name, gameId)).rejects.toThrowError(
         "Response status: 404"
