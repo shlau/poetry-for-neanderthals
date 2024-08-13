e6cc495d68e05579dcc9f72707f4edd1ac331cf0
sheldon <sheldonlau1@gmail.com>
2024-08-12 22:57:13 -0700

    add option to collapse chat

--- client-react/src/game/chat/Chat.tsx
+++ client-react/src/game/chat/Chat.tsx
@@ -1,7 +1,9 @@
 import { TextField } from "@mui/material";
 import "./Chat.less";
-import { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import { Team, User } from "../../models/User.model";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 
 export interface ChatMessage {
   text: string;
@@ -21,6 +23,7 @@ export default function Chat({
   currentUser,
   poetId,
 }: ChatProps) {
+  const [chatOpen, setChatOpen] = useState(true);
   const [currentChatMessage, setCurrentChatMessage] = useState("");
   const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
     if (e.key === "Enter") {
@@ -29,34 +32,51 @@ export default function Chat({
     }
   };
   return (
-    <div className="chat-box">
-      <div className="chat-history">
-        {chatMessages.map((msg: ChatMessage, idx: number) => {
-          return (
-            <div className="chat-message" key={idx}>
-              <p
-                className={`name ${msg.team === Team.BLUE ? "blue" : "red"} ${
-                  currentUser.id === msg.id ? "current" : ""
-                } ${poetId === msg.id ? "orange" : ""}`}
-              >
-                {msg.name}:
-              </p>
-              <p className="text">{msg.text}</p>
-            </div>
-          );
-        })}
-      </div>
-      <TextField
-        className="chat-input"
-        id="outlined-basic"
-        label="Message"
-        variant="outlined"
-        value={currentChatMessage}
-        onChange={(e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
-          setCurrentChatMessage(e.currentTarget.value)
-        }
-        onKeyDown={handleKeyDown}
-      />
-    </div>
+    <React.Fragment>
+      {chatOpen && (
+        <div className="chat-box chat-open">
+          <div className="header">
+            <ChevronRightIcon onClick={() => setChatOpen(false)} />
+          </div>
+          <div className="chat-history">
+            {chatMessages.map((msg: ChatMessage, idx: number) => {
+              return (
+                <div className="chat-message" key={idx}>
+                  <p
+                    className={`name ${
+                      msg.team === Team.BLUE ? "blue" : "red"
+                    } ${currentUser.id === msg.id ? "current" : ""} ${
+                      poetId === msg.id ? "orange" : ""
+                    }`}
+                  >
+                    {msg.name}:
+                  </p>
+                  <p className="text">{msg.text}</p>
+                </div>
+              );
+            })}
+          </div>
+          <TextField
+            className="chat-input"
+            id="outlined-basic"
+            label="Message"
+            variant="outlined"
+            value={currentChatMessage}
+            onChange={(
+              e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+            ) => setCurrentChatMessage(e.currentTarget.value)}
+            onKeyDown={handleKeyDown}
+          />
+        </div>
+      )}
+      {!chatOpen && (
+        <div className="chat-box chat-closed">
+          <div className="header">
+            <ChevronLeftIcon onClick={() => setChatOpen(true)} />
+          </div>
+          <div className="chat-body"></div>
+        </div>
+      )}
+    </React.Fragment>
   );
 }
